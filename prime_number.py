d09416ad08e1d75e037f327140d2cd8cb73f3ee2
unknown <bakersekitoleko@yahoo.com>
2017-04-18 23:48:18 -0700

    Modifications

--- prime_number.py
+++ prime_number.py
@@ -4,20 +4,25 @@ class PrimeNumber():
         This function has linear time complexity
         Respresented by N(O)
     '''
-    def generate_prime_number(self, n):
+
+    def generate_prime_numbers(self, number):
+        prime_numbers = []
         try:
-            if n < 0:
+            if number < 0:
                 return "Only positive integers allowed"
-            if n == 2 or n == 3:
-                return n
-            flag = 0
-            for i in range(2, n):
-                if n % i == 0:
-                    flag = 1
-                    break
-            if flag == 0 and n > 1:
-                return n
-            else:
-                return "Not prime number"
+            if number == 1:
+                return 'Empty list of prime numbers'
+            if number == 2:
+                prime_numbers.append(number)
+                return prime_numbers
+            for x in range(number):
+                flag = 0
+                for i in range(2, x):
+                    if x % i == 0 and x != 2:
+                        flag = 1
+                        break
+                if flag == 0 and x > 1:
+                    prime_numbers.append(x)
+            return prime_numbers
         except TypeError:
             raise "Not integer"
