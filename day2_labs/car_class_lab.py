5ee7179a63474cff17b5fd52b88b6d7acfb1f153
unknown <bakersekitoleko@yahoo.com>
2017-04-20 21:09:27 -0700

    day2_labs

--- day2_labs/car_class_lab.py
+++ day2_labs/car_class_lab.py
@@ -1,17 +1,31 @@
-
 class Car():
 
-    def __init__(self, car_type, car_model, car_name):
-        self.num_of_wheels = 4
+    def __init__(self, name='General', model='GM', car_type='saloon'):
+        self.car_type = car_type
+        self.model = model
+        self.name = name
         self.speed = 0
-        if car_name is None:
-            self.car_name = 'General'
-        if car_model is None:
-            self.car_model = 'GM'
+        self.num_of_doors = 4
+        self.num_of_wheels = 4
 
+        if self.name == 'Porshe' or self.name == 'Koenigsegg':
+            self.num_of_doors = 2
 
-    def drive(self, speed):
-        car = Car('MAN', 'Truck', 'trailer')
-        self.speed = speed
-        return car
+        if self.car_type == 'trailer':
+            self.num_of_wheels = 8
+
+    def is_saloon(self):
+        if self.car_type == 'saloon':
+            return True
+        else:
+            return False
+
+    def drive(self, gear):
+        if self.car_type == 'trailer':
+            self.speed = gear * 11
+            return self
+        else:
+            self.speed = 10**gear
+            return self
+
 
