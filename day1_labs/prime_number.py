6712ead4b4e290d9df5e204f09403b21dd8af6ac
unknown <bakersekitoleko@yahoo.com>
2017-04-25 15:29:28 -0700

    day1_labs

--- day1_labs/prime_number.py
+++ day1_labs/prime_number.py
@@ -1,17 +1,26 @@
-class PrimeNumber():
-
-    def generate_prime_number(self, n):
-        if n == 2 or n == 3:
-            return n
-        flag = 0
-        for i in range(2, n):
-            if n % i == 0:
-                flag = 1
-                break
-        if flag == 0 and n > 1:
-            return n
-
 '''
-if __name__ == '__main__':
-    objj = PrimeNumber()
-    print(objj.generate_prime_number(1)) '''
+    This function has linear time complexity
+    Respresented by N(O)
+'''
+
+def generate_prime_numbers(number):
+    prime_numbers = []
+    try:
+        if number < 0:
+            return "Only positive integers allowed"
+        if number == 1:
+            return 'Empty list of prime numbers'
+        if number == 2:
+            prime_numbers.append(number)
+            return prime_numbers
+        for x in range(number):
+            flag = 0
+            for i in range(2, x):
+                if x % i == 0 and x != 2:
+                    flag = 1
+                    break
+            if flag == 0 and x > 1:
+                prime_numbers.append(x)
+        return prime_numbers
+    except TypeError:
+        raise "Not integer"
--- day1_labs/prime_number_test.py
+++ day1_labs/prime_number_test.py
@@ -1,15 +1,32 @@
 import unittest
-from prime_number import PrimeNumber
+from prime_number import generate_prime_numbers
+
 
 class PrimeNumberTestCases(unittest.TestCase):
 
-    def setUp(self):
-        self.obj = PrimeNumber()
+    def test_negative_number(self):
+        result = generate_prime_numbers(-5)
+        self.assertEqual(
+            'Only positive integers allowed',
+            result,
+            msg='The input number should be a positive integer')
 
-    def test_nonprime_number(self):
-        result = self.obj.generate_prime_number(4)
-        if result != 2:
-            self.assertNotEqual(result % 2, 0)
+    def test_for_empty_list(self):
+        result = generate_prime_numbers(1)
+        self.assertEqual(
+            'Empty list of prime numbers',
+            result,
+            msg='The input number should be greater one')
+
+    def test_if_list_is_generated(self):
+        result = generate_prime_numbers(10)
+        self.assertEqual(
+            [2, 3, 5, 7],
+            result,
+            msg='To generate a list of prime numbers, the input should be greater than 1')
+
+    def test_error_on_wrong_input(self):
+        self.assertRaises(TypeError, generate_prime_numbers, '5')
 
 
 if __name__ == '__main__':
